fde28074e0a7f4a875a7aedf336f608b89a486ed
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-04-04 20:35:05 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/misrshoryanelataa/misr_shoryan_elataa/MisrShoryanElataaApplication.java
@@ -0,0 +1,13 @@
+package com.misrshoryanelataa.misr_shoryan_elataa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisrShoryanElataaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisrShoryanElataaApplication.class, args);
+	}
+
+}
